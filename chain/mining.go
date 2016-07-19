2495f922f572f970d68598b639ad9563da882049
Lorenzo Setale <lorenzo@setale.me>
2016-07-19 20:35:50 +0200

    revert the last change and explain why

--- chain/mining.go
+++ chain/mining.go
@@ -2,19 +2,6 @@ package chain
 
 import "math/rand"
 
-func miningThread(b *Block, difficulty float64) *Block {
-	for {
-		newNonce := rand.Uint32()
-		b.Nonce = newNonce
-
-		if b.IsHashValid(difficulty) {
-			break
-		}
-	}
-
-	return b
-}
-
 // Mine will try to find a valid random nonce
 func (b *Block) Mine(difficulty float64) *Block {
 	// Check if a valid nonce is already there
@@ -28,6 +15,16 @@ func (b *Block) Mine(difficulty float64) *Block {
 	}
 
 	// Until the block has a valid hash, generate a new nonce
-	b = miningThread(b, difficulty)
+	// This is not using another method, nor a thread/goroutine, nor threads
+	// because this code is designed to have fun
+	for {
+		newNonce := rand.Uint32()
+		b.Nonce = newNonce
+
+		if b.IsHashValid(difficulty) {
+			break
+		}
+	}
+
 	return b
 }
