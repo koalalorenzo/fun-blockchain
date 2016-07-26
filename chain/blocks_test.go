da3032ad3af5a32f16a64113c279409b2628d889
Lorenzo Setale <lorenzo@setale.me>
2016-07-26 20:59:56 +0200

    adds some tests for block validation

--- chain/blocks_test.go
+++ chain/blocks_test.go
@@ -33,8 +33,12 @@ func TestBlockExportImportHex(t *testing.T) {
 		t.Error("Signature check failed")
 	}
 
-	if testBlock.IsHashValid(0) != newBlock.IsHashValid(0) {
+	if testBlock.IsHashValid(MinBlockDifficulty) != newBlock.IsHashValid(MinBlockDifficulty) {
 		t.Error("Hash validation is different")
+	}
+
+	if testBlock.IsHashValid(MinBlockDifficulty) == true {
+		t.Error("Block hash should be invalid")
 	}
 }
 
@@ -52,11 +56,19 @@ func TestBlockHashString(t *testing.T) {
 
 func TestBlockValidation(t *testing.T) {
 
-	hex := "313436393535363232313636383731383134362c322c332c333533343337333533363335333233303334363133373335333636333332333033333332333333363332333033333332333333303333363133333330333333333333363133333334333333313332333033343333333433353335333333353334333233303333333233333330333333313333333632633337333733363636333636363337333732633337333433373332333733353337333333373334333233303336363433363335"
-
+	hex := "313436393535363937373332333032313136372c353537373030363739313934373737393431302c6e6f7065"
+	hash := "739ed3d815b7af4b0e75e314adcb562f3a7b40651b0a993b0b9d01b98e0f7b01"
 	block := BlockFromHex(hex)
+
+	if block.HashString() != hash {
+		t.Error("Block different")
+	}
 
 	if block.IsContentValid() != true {
 		t.Error("Block content not valid")
+	}
+
+	if block.IsHashValid(MinBlockDifficulty) != true {
+		t.Error("Block hash not acceptable")
 	}
 }
